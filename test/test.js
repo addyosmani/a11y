61e1a8ca4d09551ea975fb4792c5fcf82fd0644b
Kenny Wang and Matt Royal <kwang@pivotal.io>
2015-07-21 10:25:42 -0700

    Remove order-dependency in tests to avoid false failures

--- test/test.js
+++ test/test.js
@@ -6,6 +6,13 @@ var a11y = require('../');
 
 process.chdir(__dirname);
 
+
+function auditsWithHeader(reports, header) {
+    return reports.audit.filter(function(aud) {
+        return aud.heading === header;
+    });
+};
+
 test('test that an empty URL fails', function (t) {
     t.plan(1);
     try {
@@ -27,24 +34,27 @@ test('fail if no callback is supplied', function (t) {
 test('test local input generates a report if callback is second param', function (t) {
     t.plan(2);
     a11y('fixture.html', function (err, reports) {
-        t.assert(reports.audit[1].heading === 'ARIA state and property values must be valid');
-        t.assert(reports.audit[1].result === 'FAIL');
+        var ariaReports = auditsWithHeader(reports ,'ARIA state and property values must be valid');
+        t.assert(ariaReports.length === 1);
+        t.assert(ariaReports[0] && ariaReports[0].result === 'FAIL');
     });
 });
 
 test('test local input generates a report if callback is third param', function (t) {
     t.plan(2);
     a11y('fixture.html', null, function (err, reports) {
-        t.assert(reports.audit[1].heading === 'ARIA state and property values must be valid');
-        t.assert(reports.audit[1].result === 'FAIL');
+        var ariaReports = auditsWithHeader(reports, 'ARIA state and property values must be valid');
+        t.assert(ariaReports.length === 1);
+        t.assert(ariaReports[0] && ariaReports[0].result === 'FAIL');
     });
 });
 
 test('test local input generates a report requiring a delay', function (t) {
     t.plan(2);
     a11y('fixture.html', {delay: 5} , function (err, reports) {
-        t.assert(reports.audit[8].heading === 'role=main should only appear on significant elements');
-        t.assert(reports.audit[8].result === 'FAIL');
+        var delayReports = auditsWithHeader(reports, 'role=main should only appear on significant elements');
+        t.assert(delayReports.length === 1);
+        t.assert(delayReports[0] && delayReports[0].result === 'FAIL');
     });
 });
 
