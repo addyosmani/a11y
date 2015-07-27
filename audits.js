6cc3af04450218910b01305a7f395b37db61999d
Caroline Taymor <ctaymor@pivotal.io>
2015-07-27 16:18:54 -0700

    Polyfill bind before running audits
    closes #44
    
    Signed-off-by: Geoff Pleiss <gpleiss@pivotal.io> (Closes #47)

--- audits.js
+++ audits.js
@@ -6,6 +6,7 @@ var webpage = require('webpage').create();
 var opts = JSON.parse(system.args[1]);
 var PAGE_TIMEOUT = 9000;
 var TOOLS_PATH = 'node_modules/accessibility-developer-tools/dist/js/axs_testing.js';
+var BIND_POLYFILL_PATH = 'node_modules/phantomjs-polyfill/bind-polyfill.js';
 
 function formatTrace(trace) {
     var src = trace.file || trace.sourceURL;
@@ -16,6 +17,11 @@ function formatTrace(trace) {
 // console.error is broken in PhantomJS
 console.error = function () {
     system.stderr.writeLine([].slice.call(arguments).join(' '));
+};
+
+// Need to polyfill bind...
+webpage.onInitialized = function () {
+  webpage.injectJs(BIND_POLYFILL_PATH);
 };
 
 webpage.settings.resourceTimeout = PAGE_TIMEOUT;
@@ -88,7 +94,12 @@ webpage.open(opts.url, function (status) {
         }
 
         console.log(JSON.stringify(ret));
-        phantom.exit();
+        // Must do crazy song and dance to get phantom to exit properly
+        // https://github.com/ariya/phantomjs/issues/12697
+        webpage.close();
+        setTimeout(function() {
+            phantom.exit();
+        }, 0);
     }, opts.delay * 1000);
 
 });
