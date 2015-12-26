5b955ce6cbc960dd4d391b20eaa1e92ff4c302e5
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-26 03:24:36 +0100

    fix tests for latest AVA version

--- test/test.js
+++ test/test.js
@@ -14,65 +14,68 @@ test('test that an empty URL fails', function (t) {
     t.throws(function () {
         a11y('', function () {});
     }, /Please supply at least one URL/);
-    t.end();
 });
 
 test('fail if no callback is supplied', function (t) {
     t.throws(function () {
         a11y('', function () {});
     });
-    t.end();
 });
 
-test('test local input generates a report if callback is second param', function (t) {
+test.cb('test local input generates a report if callback is second param', function (t) {
     t.plan(3);
 
     a11y('fixture.html', function (err, reports) {
-        t.error(err);
+        t.ifError(err);
         var ariaReports = auditsWithHeader(reports, 'ARIA state and property values must be valid');
         t.is(ariaReports.length, 1);
         t.is(ariaReports[0] && ariaReports[0].result, 'FAIL');
+        t.end();
     });
 });
 
-test('test local input generates a report if callback is third param', function (t) {
+test.cb('test local input generates a report if callback is third param', function (t) {
     t.plan(3);
 
     a11y('fixture.html', null, function (err, reports) {
-        t.error(err);
+        t.ifError(err);
         var ariaReports = auditsWithHeader(reports, 'ARIA state and property values must be valid');
         t.is(ariaReports.length, 1);
         t.is(ariaReports[0] && ariaReports[0].result, 'FAIL');
+        t.end();
     });
 });
 
-test('test local input generates a report requiring a delay', function (t) {
+test.cb('test local input generates a report requiring a delay', function (t) {
     t.plan(3);
 
     a11y('fixture.html', {delay: 5}, function (err, reports) {
-        t.error(err);
+        t.ifError(err);
         var delayReports = auditsWithHeader(reports, 'role=main should only appear on significant elements');
         t.is(delayReports.length, 1);
         t.is(delayReports[0] && delayReports[0].result, 'FAIL');
+        t.end();
     });
 });
 
-test('test local input generates a verbose report', function (t) {
+test.cb('test local input generates a verbose report', function (t) {
     t.plan(2);
 
     a11y('fixture.html', {verbose: true}, function (err, reports) {
-        t.error(err);
+        t.ifError(err);
         t.true(reports.report.indexOf('*** Begin accessibility audit results ***') !== -1);
+        t.end();
     });
 });
 
-test('test local input generates a report that includes all failures for a given violation', function (t) {
+test.cb('test local input generates a report that includes all failures for a given violation', function (t) {
     t.plan(3);
 
     a11y('fixture.html', function (err, reports) {
-        t.error(err);
+        t.ifError(err);
         var matchingReports = auditsWithHeader(reports, 'Images should have a text alternative or presentational role');
         t.is(matchingReports.length, 1);
         t.is(matchingReports[0] && matchingReports[0].elements.match(/\n/g).length, 7);
+        t.end();
     });
 });
