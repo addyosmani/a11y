4fbb6da312bcf78eaf8c45200d24daf49236f47c
marob <marob@smile.fr>
2016-04-25 17:23:19 +0200

    FIX linting

--- junitReporter.js
+++ junitReporter.js
@@ -1,12 +1,12 @@
 'use strict';
-var junitReportBuilder = require('junit-report-builder');
 var path = require('path');
+var junitReportBuilder = require('junit-report-builder');
 
-module.exports = function(url, audit, timestamp, time) {
+module.exports = function (url, audit, timestamp, time) {
     var builder = junitReportBuilder.newBuilder();
 
     var fileName = url;
-    var isOnFileSystem = fileName.search(/^file:\/\/|^\\/) != -1;
+    var isOnFileSystem = fileName.search(/^file:\/\/|^\\/) !== -1;
     if (isOnFileSystem) {
         var pathToFile = path.resolve(fileName.replace(/^file:\/\/|^\\/, ''));
         fileName = path.relative(process.cwd(), pathToFile);
@@ -16,12 +16,12 @@ module.exports = function(url, audit, timestamp, time) {
         .name(fileName)
         .timestamp(timestamp)
         .time(time);
-    audit.forEach(function(result) {
+    audit.forEach(function (result) {
         var name = result.code;
-        if(result.result === 'FAIL'){
+        if (result.result === 'FAIL') {
             var failure = result.heading + ' (' + result.url + ')\n';
 
-            var stacktrace = 'Severity: ' + result.severity + '\n\n'+
+            var stacktrace = 'Severity: ' + result.severity + '\n\n' +
                 'Target:\n' +
                 result.elements.replace(/\n/g, '\n    \u2022 ');
 
@@ -30,7 +30,7 @@ module.exports = function(url, audit, timestamp, time) {
                 .failure(failure)
                 .stacktrace(stacktrace);
         }
-        if(result.result === 'PASS' || result.result === 'NA'){
+        if (result.result === 'PASS' || result.result === 'NA') {
             suite.testCase()
                 .name(name);
         }
